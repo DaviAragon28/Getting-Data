02351a4b99b520c8e77f409f507329c3b15a0ad2
David Aragón <Davidaragon0007@gmail.com>
2022-04-25 15:22:13 -0500

    Primer commit de todo el prooyecto

---
+++ main.js
@@ -0,0 +1,201 @@
+//Código cuadrado
+function perimetroCuadrado(lado) {
+  return lado * 4;
+}
+
+function areaCuadrado(lado) {
+  return lado * lado;
+}
+
+//Código rectangulo
+
+function periRectangulo(lado, base) {
+  return lado * 2 + base * 2;
+}
+
+function areaRectangulo(lado, base) {
+  return lado * base;
+}
+
+//Código triangulo
+
+function perimetroTriangulo(lado) {
+  return lado * 3;
+}
+
+function alturaTriangulo(altura) {
+  return altura;
+}
+
+function areaTriangulo(base, altura) {
+  return (base * altura) / 2;
+}
+
+//Código circulo
+
+function radioCirculo(radio) {
+  return radio;
+}
+
+function diametroCirculo(radio) {
+  return radio * 2;
+}
+
+const PI = Math.PI;
+
+function perimetroCirculo(radio) {
+  const diametro = diametroCirculo(radio);
+  return diametro * PI;
+}
+
+function areaCirculo(radio) {
+  return radio * radio * PI;
+}
+
+// Sincronizando con el HTML
+
+//Cuadrado
+function calcularPerimetroCuadrado() {
+  const input = document.getElementById('inputCuadrado');
+  const value = input.value;
+  const perimetro = perimetroCuadrado(value);
+
+  const parrafo = document.querySelector('.textoDesdeJSCuadrado');
+  parrafo.innerText = `El perímetro del cuadrado es: ${perimetro}cm`;
+  const results = document.querySelector('.result');
+  results.innerText = `${value}cm`;
+  const resultRight = document.querySelector('.result-right');
+  resultRight.innerText = `${value}cm`;
+  const resultLeft = document.querySelector('.result-left');
+  resultLeft.innerText = `${value}cm`;
+  const resultBottom = document.querySelector('.result-bottom');
+  resultBottom.innerText = `${value}cm`;
+}
+function calcularAreaCuadrado() {
+  const input = document.getElementById('inputCuadrado');
+  const value = input.value;
+  const area = areaCuadrado(value);
+
+  const parrafo = document.querySelector('.textoDesdeJSCuadrado');
+  parrafo.innerText = `El area del cuadrado es: ${area} cm²`;
+  const results = document.querySelector('.result');
+  results.innerText = `${value}cm`;
+  const resultRight = document.querySelector('.result-right');
+  resultRight.innerText = `${value}cm`;
+  const resultLeft = document.querySelector('.result-left');
+  resultLeft.innerText = `${value}cm`;
+  const resultBottom = document.querySelector('.result-bottom');
+  resultBottom.innerText = `${value}cm`;
+}
+
+//Rectangulo
+function calcularPerimetroRectangulo3() {
+  const rectangulo = document.getElementById('inputRectangulo');
+  const value = rectangulo.value;
+
+  const rectangulo2 = document.getElementById('inputRectangulo2');
+  const value2 = rectangulo2.value;
+
+  const perimetroRec = periRectangulo(value2, value);
+
+  const parrafo = document.querySelector('.textoDesdeJSRectangulo');
+  parrafo.innerText = `El perímetro del rectangulo es: ${perimetroRec}cm`;
+  const results = document.querySelector('.result');
+  results.innerText = `${value2}cm`;
+  const resultRight = document.querySelector('.result-right');
+  resultRight.innerText = `${value}cm`;
+  const resultLeft = document.querySelector('.result-left');
+  resultLeft.innerText = `${value}cm`;
+  const resultBottom = document.querySelector('.result-bottom');
+  resultBottom.innerText = `${value2}cm`;
+}
+function calcularAreaRectangulo() {
+  const inputRectangulo = document.getElementById('inputRectangulo');
+  const value = inputRectangulo.value;
+
+  const inputRectangulo2 = document.getElementById('inputRectangulo2');
+  const value2 = inputRectangulo2.value;
+
+  const areaRec = areaRectangulo(value, value2);
+
+  const parrafo = document.querySelector('.textoDesdeJSRectangulo');
+  parrafo.innerText = `El area del rectangulo es: ${areaRec} cm²`;
+  const results = document.querySelector('.result');
+  results.innerText = `${value2}cm`;
+  const resultRight = document.querySelector('.result-right');
+  resultRight.innerText = `${value}cm`;
+  const resultLeft = document.querySelector('.result-left');
+  resultLeft.innerText = `${value}cm`;
+  const resultBottom = document.querySelector('.result-bottom');
+  resultBottom.innerText = `${value2}cm`;
+}
+
+//Circulo
+function calcularPerimetroCirculo() {
+  const inputCirculo = document.querySelector('#inputCirculo');
+  const valor = inputCirculo.value;
+
+  const perimetroCir = perimetroCirculo(valor);
+
+  const parrafo = document.querySelector('.textoDesdeJSCirculo');
+  parrafo.innerText = `El perímetro del circulo es: ${parseInt(
+    perimetroCir
+  )} cm`;
+}
+function calcularAreaCirculo() {
+  const inputCirculo = document.querySelector('#inputCirculo');
+  const valor = inputCirculo.value;
+
+  const areaCir = areaCirculo(valor);
+
+  const parrafo = document.querySelector('.textoDesdeJSCirculo');
+  parrafo.innerText = `El área del circulo es: ${parseInt(areaCir)} cm²`;
+}
+function calcularDiametroCirculo() {
+  const inputCirculo = document.querySelector('#inputCirculo');
+  const valor = inputCirculo.value;
+
+  const diametroCir = diametroCirculo(valor);
+
+  const parrafo = document.querySelector('.textoDesdeJSCirculo');
+  parrafo.innerText = `El diametro del circulo es: ${diametroCir} cm`;
+}
+
+//Triangulo
+function calcularPerimetroTriangulo() {
+  const inputTriangulo = document.querySelector('#inputTriangulo');
+  const valor = inputTriangulo.value;
+
+  const perimetroTri = perimetroTriangulo(valor);
+
+  const parrafo = document.querySelector('.textoDesdeJSTriangulo');
+  parrafo.innerText = `El perimetro del triangulo es ${perimetroTri}cm`;
+}
+function calcularAreaTriangulo() {
+  const inputTriangulo = document.querySelector('#inputTriangulo');
+  const valor = inputTriangulo.value;
+
+  const lado = valor ** 2;
+  const base1 = valor / 2;
+  const base2 = base1 ** 2;
+  const resta = lado - base2;
+  const valorFinal = Math.sqrt(resta);
+
+  const areaTri = areaTriangulo(valor, valorFinal);
+  const parrafo = document.querySelector('.textoDesdeJSTriangulo');
+  parrafo.innerText = `El área del triangulo es ${areaTri.toFixed(2)}cm²`;
+}
+function calcularAlturaTriangulo() {
+  const inputTriangulo = document.querySelector('#inputTriangulo');
+  const valor = inputTriangulo.value;
+
+  const lado = valor ** 2;
+  const base1 = valor / 2;
+  const base2 = base1 ** 2;
+  const resta = lado - base2;
+  const valorFinal = Math.sqrt(resta);
+  valorFinal.toFixed(2);
+
+  const parrafo = document.querySelector('.textoDesdeJSTriangulo');
+  parrafo.innerText = `La altura del triangulo es: ${valorFinal.toFixed(2)}cm`;
+}
